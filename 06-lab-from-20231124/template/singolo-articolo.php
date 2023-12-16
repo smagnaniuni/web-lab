7e8daea6be778cb9aa14d11cd7ec63b89bc4648a
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-12-16 12:37:56 +0100

    fix upload image dir

--- 06-lab-from-20231124/template/singolo-articolo.php
+++ 06-lab-from-20231124/template/singolo-articolo.php
@@ -10,7 +10,7 @@ else
 
 <article>
     <header>
-        <img src="<?php echo UPLOAD_DIR.$articolo["imgarticolo"]; ?>" alt="">
+        <img src="<?php echo UPLOAD_DIR . "img/" . $articolo["imgarticolo"]; ?>" alt="">
         <h2><?php echo $articolo["titoloarticolo"]; ?></h2>
         <p><?php echo $articolo["dataarticolo"] . " - " . $articolo["nome"]; ?></p>
     </header>
